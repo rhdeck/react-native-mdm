7d2eb9f61b8c32937b80690b02d2e5e2dbbfbc56
Ray Deck <ray@raydeck.com>
2018-02-16 13:27:41 -0500

    Making all function calls explicit so they are easier to see from calling app.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 "use strict";
-
 import { DeviceEventEmitter, NativeModules } from "react-native";
-
 const { MobileDeviceManager } = NativeModules;
+async function isSupported() {
+  return await MobileDeviceManager.isSupported();
+}
+async function getConfiguration() {
+  return await MobileDeviceManager.getConfiguration();
+}
+function addListener(callback) {
+  return DeviceEventEmitter.addListener(
+    MobileDeviceManager.APP_CONFIG_CHANGED,
+    callback
+  );
+}
+async function isAutonomousSingleAppModeSupported() {
+  return await MobileDeviceMananger.isAutonomousSingleAppModeSupported();
+}
+async function isSingleAppModeEnabled() {
+  return await MobileDeviceMananger.isSingleAppModeEnabled();
+}
+async function isAutonomousSingleAppModeEnabled() {
+  return await MobileDeviceMananger.isAutonomousSingleAppModeEnabled();
+}
+async function enableAutonomousSingleAppMode() {
+  return await MobileDeviceMananger.enableAutonomousSingleAppMode();
+}
+async function disableAutonomousSingleAppMode() {
+  return await MobileDeviceManager.disableAutonomousSingleAppMode();
+}
 
 export default {
-  ...MobileDeviceManager,
-  addListener(callback) {
-    return DeviceEventEmitter.addListener(
-      MobileDeviceManager.APP_CONFIG_CHANGED,
-      callback
-    );
-  }
+  isSupported,
+  getConfiguration,
+  addListener,
+  isAutonomousSingleAppModeSupported,
+  isAutonomousSingleAppModeEnabled,
+  isSingleAppModeEnabled,
+  enableAutonomousSingleAppMode,
+  disableAutonomousSingleAppMode
 };
